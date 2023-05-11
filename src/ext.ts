4a0f82d5a35464cc23387d07c7e07ec189b5d6a7
Yoshiya Hinosawa <stibium121@gmail.com>
2023-05-11 12:25:25 +0900

    chore: move files to src dir

--- src/ext.ts
+++ src/ext.ts
@@ -274,7 +274,7 @@ class PixelEditProvider implements CustomEditorProvider<PixelEditDocument> {
 
     // Setup initial content for the webview
     webview.options = { enableScripts: true }
-    const scriptUri = webview.asWebviewUri(Uri.joinPath(this.#uri, "edit.js"))
+    const scriptUri = webview.asWebviewUri(Uri.joinPath(this.#uri, "src/edit.js"))
     webview.html = /* html */ `
     <html>
       <head>
